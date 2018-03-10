55ba9e9d18aa4ca35c104e4bf8212b0f4aa1a8d9
TweedleTween <nappingbird0304@gmail.com>
2018-03-09 19:25:54 -0800

    Modify fix data

--- 1-get_author_paper_list/fix.py
+++ 1-get_author_paper_list/fix.py
@@ -1,14 +1,9 @@
-file1 = open('tmp.csv', 'r')
-
-file2 = open('aaai.csv','w')
-file3 = open('to_fix.csv','w')
+file1 = open('aaai.csv', 'r')
+file2 = open('to_fix.csv','w')
 
 for line in file1.readlines():
     if line.startswith(','):
-        file3.write(line)
-    else:
         file2.write(line)
 
 file1.close()
 file2.close()
-file3.close()
